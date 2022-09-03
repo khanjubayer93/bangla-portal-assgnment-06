da93a8eed4314d962f78d2730cc104fb020ff29b
khanjubayer93 <jubayerkhan711@gmail.com>
2022-09-03 23:57:24 +0600

    body added

--- js/index.js
+++ js/index.js
@@ -14,6 +14,7 @@ const categoryBar = async () => {
         const { category_name, category_id } = news;
         // console.log(category_id);
         const categoryUl = document.createElement('ul');
+        categoryUl.classList.add('menu')
         categoryUl.innerHTML = `
         <li onclick="loadNewsDetails(${category_id})"><a>${category_name}</a></li>
         `;
@@ -30,46 +31,46 @@ const loadNewsDetails = async (code) => {
 }
 
 const displayNews = detail => {
-    console.log(detail);
     const newsFeed = document.getElementById('news-feed');
     newsFeed.innerHTML = '';
 
+
     detail.sort((a, b) => b.total_view - a.total_view);
-    const { author, category_id, details, image_url, others_info, rating, thumbnail_url, title, total_view, _id } = news;
-    const { img, name, published_date } = author;
     detail.forEach(news => {
+        const { title, thumbnail_url, details, total_view } = news;
+        const { img, name, published_date } = news.author;
+
         const newsDiv = document.createElement('div');
-        console.log(news);
+        newsDiv.classList.add('news')
         newsDiv.innerHTML = `
     
-    <div id="news-feed" class="card card-side bg-base-100 shadow-xl">
-                <figure class="p-5"><img src="https://placeimg.com/200/280/arch" alt="Movie"></figure>
+    <div id="news-feed" class="card card-side bg-base-100 shadow-xl flex justify-between w-full mb-4">
+                <figure class="p-5 w-1/2"><img src="${thumbnail_url}" alt=""></figure>
                 <div class="card-body">
-                    <h2 class="card-title">New movie is released!</h2>
-                    <p>Click the button to watch on Jetflix app.</p>
-                    <div class="card-actions flex-justify-btween">
-                        <div class="flex">
+                    <h2 class="card-title">${title}</h2>
+                    <p>${details.slice(0, 500) + '...'}</p>
+                    <div class="card-actions">
+                        <div class="">
                             <div class="pr-2">
-                                <img class="w-10 rounded-full" src="" alt="">
+                                <img class="w-10 rounded-full" src="${img}" alt="">
                             </div>
                             <div>
-                                <h6 class="font-medium">Jone Coper</h6>
-                                <p>Jan, 10, 2022</p>
+                                <h6 class="font-medium">${name}</h6>
+                                <p>${published_date}</p>
                             </div>
                         </div>
-                        <div>
-
-                            <p><i class="fa-regular fa-eye"></i><span class="font-semibold"> 1.5M</span>
+                        <div class="">
+                            <p><i class="fa-regular fa-eye"></i><span class="font-semibold"> ${total_view}M</span>
                             </p>
                         </div>
-                        <div>
+                        <div class="">
                             <i class="fa-solid fa-star"></i>
                             <i class="fa-solid fa-star"></i>
                             <i class="fa-solid fa-star"></i>
                             <i class="fa-solid fa-star"></i>
                             <i class="fa-solid fa-star-half-stroke"></i>
                         </div>
-                        <div><button class="btn btn-outline btn-info">Lern More</button></div>
+                        <div class="btn btn-outline btn-info "><button>Lern More</button></div>
                     </div>
                 </div>
             </div>
